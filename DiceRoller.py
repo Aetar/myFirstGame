b897c61c74e82dd7ffed6f12f259394291681fa9
Indronil <indronil@live.in>
2015-06-28 00:40:20 +0530

    Update and rename README.md to DiceRoller.py

---
+++ DiceRoller.py
@@ -0,0 +1,4 @@
+#This is my first program something more advance then hello world i guess
+#hope you all will lke it and help me improve it 
+#features i want to have 
+
--- README.md
+++
@@ -1,2 +0,0 @@
-# myFirstGame
-This is my first program something more advance then hello world i guess
